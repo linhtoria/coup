d6ac1f23916820ef432cc92889b2f01136ceaf59
Victoria Nguyen <nguyen.victoriais@gmail.com>
2019-04-28 00:38:12 -0500

    Automatic restarts server with nodemom

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Node on Divio!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
 
 app.listen(3000, function () {
   console.log('Listening on port 3000')
